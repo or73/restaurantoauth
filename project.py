8358ccb00bf3bcf0a11edc2a94eaf98afe2eb474
OR73 <oreyesc@gmail.com>
2019-04-21 12:45:41 -0500

    OAuth authentication has been added - A 'client_secrets.json' includes all data provided by Google console developers to validate OAuth credentials - In 'login.html' a 'login' button has been added, with several data to validate user session and authenticate. A POST message is send by 'AJAX' message, including the 'STATE' or validation session code generated by the app. - A 'gconnect' link is added, with a 'gconnect' function to:     - Validate token state, and upgrade the authorization code into a credentials object     - Validate that the access token is valid, if there was an error in the access token info then session is aborted     - Verify that the access token is used for the intended user     - Verify that the access token is valid for this app     - Store the access token in the session, for later use     - Get user info

--- project.py
+++ project.py
@@ -1,14 +1,28 @@
 from flask import flash, Flask, jsonify, make_response, redirect, render_template, request,  url_for
-
+# ------------------------ Database
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
-
+# ------------------------ HTTP Authorization
 from flask import session as login_session
 from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
-import httplib2, json, random, requests, string
+import httplib2
+import json
+import random
+import requests
+import string
 
-CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+
+# This variable specifies the name of a file that contains the OAuth 2.0
+# information for this application, including its client_id and client_secret.
+CLIENT_SECRETS_FILE = 'client_secrets.json'
+
+CLIENT_ID = json.loads(
+    open(CLIENT_SECRETS_FILE, 'r').read())['web']['client_id']
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+APPLICATION_NAME = "Restaurant Menu Application"
 
 app = Flask(__name__)
 
@@ -19,11 +33,13 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 # Create a state token to prevent request forgery.
 # Store it in the session for later validation
 @app.route('/login')
 def showLogin():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in range(32))
     login_session['state'] = state
     # return 'The current session state is %s' % login_session['state']
     print('The current session state is %s' % login_session['state'])
@@ -32,80 +48,87 @@ def showLogin():
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
-    if requests.args.get('state') != login_session['state']:
-        response = make_response(json.dumps('Invalid state parameter'), 401)
-        response.headers['Content-type'] = 'application/json'
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
         return response
+    # Obtain authorization code
     code = request.data
 
     try:
-        # Upgrade the authorization code into a credential object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE, scope='')
+        print('oauth_flow: ', oauth_flow)
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
-        response = make_response(json.dumps('Failed to upgrade the authorization code'), 401)
-        response.headers['Content-type'] = 'application/json'
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Check that the access token is valid
+    # Check that the access token is valid.
     access_token = credentials.access_token
-    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_tokens=%s' % access_token)
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
-
+    # If there was an error in the access token info, abort.
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
-        response.header['Content-type'] = 'application/json'
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
-    # Verify that the access token is used for the intended user
+    # Verify that the access token is used for the intended user.
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
-        response = make_response(json.dumps('Token\'s user ID doesn\'t match given user ID.'), 401)
-        response.headers['Content-type'] = 'application/json'
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Verify that the access token is valid for this app
+    # Verify that the access token is valid for this app.
     if result['issued_to'] != CLIENT_ID:
-        response = make_response(json.dumps('Token\'s client DI does not match'), 401)
-        print('Token\'s client ID does not match app\'s')
-        response.headers['Content-type'] = 'application/json'
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print('Token\'s client ID does not match app\'s.')
+        response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Check to see if user is already logged in
-    stored_credentials = login_session.get('credentials')
+    stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
-    if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response =  make_response(json.dumps('Current user is already connected'), 200)
-        response.headers['Content-type'] = 'application/json'
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
-    # Store the access token in the session for later use
-    login_session['creadentials'] = credentials
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
 
     # Get user info
-    userinfo_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
-    params = { 'access_token': credentials.access_token, 'alt': 'json'}
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
-    data = json.loads(answer.text)
+
+    data = answer.json()
 
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
 
     output = ''
-    output += '<h1>Welcome '
+    output += '<h1>Welcome, '
     output += login_session['username']
-
-    output += '</h1>'
-    output += '<img src=\''
+    output += '!</h1>'
+    output += '<img src="'
     output += login_session['picture']
-    output += '\'style=\'width: 300px; height: 300px; ' \
-              'border-radius: 150px; -webkit-border-radius: ' \
-              '150px; -moz-border-radius: 150px;\'>'
-    flash('you are now logged in as %s' % login_session['username'])
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print('done!')
     return output
-
 
 
 # JSON APIs to view Restaurant Information
@@ -147,6 +170,7 @@ def newRestaurant():
       return redirect(url_for('showRestaurants'))
   else:
       return render_template('newRestaurant.html')
+
 
 # Edit a restaurant
 @app.route('/restaurant/<int:restaurant_id>/edit/', methods = ['GET', 'POST'])
