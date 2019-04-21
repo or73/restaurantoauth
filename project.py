ba9908d2f967af626cb456397ac57d99740be688
OR73 <oreyesc@gmail.com>
2019-04-20 21:02:13 -0500

    Add a javascript function called 'signInCallback', that hides the sign-ing button when the user is authorized Use jQuery to create an 'AJAX' call that passes the one time code Google gave to the client onto the server. - 'STATE': the server state is passed to the server as an argument to verify against the cross-site reference forgery attack. - 'processData' = false indicates that we do not want jQuery to process the response into a string. - 'octet-stream': indicates that we are sending an arbitrary binary stream of data. - 'charset=utf-8': indicates that it is formatted using a universal character set called Unicode. - 'data': specify the data that we are going to send along to our server the one time use code. - 'success': when we receive a 200 or a successful response code from our server, let's return a successful login message to the user, and then redirect to the main restaurant's page after about four seconds. if the server has any additional response information to pass to the client, we can present it with the 'result' variable. - 'error': in the event that an error was returned by Google, it is reported to the console.log - In the event that no response was returned by the server to the callback function, then an error message is returned to 'result' div. - Add the 'client_secrets.json' file. - ' CLIENT_ID': Reference the client secrets file (client_secrets.json). - Validate token

--- project.py
+++ project.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from flask import flash, Flask, jsonify, make_response, redirect, render_template, request,  url_for
 
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
 from flask import session as login_session
-import random, string
+from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
+import httplib2, json, random, requests, string
 
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
 
 app = Flask(__name__)
 
@@ -25,7 +27,85 @@ def showLogin():
     login_session['state'] = state
     # return 'The current session state is %s' % login_session['state']
     print('The current session state is %s' % login_session['state'])
-    return render_template('login.html')
+    return render_template('login.html', STATE=state)
+
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    if requests.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter'), 401)
+        response.headers['Content-type'] = 'application/json'
+        return response
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credential object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(json.dumps('Failed to upgrade the authorization code'), 401)
+        response.headers['Content-type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_tokens=%s' % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.header['Content-type'] = 'application/json'
+
+    # Verify that the access token is used for the intended user
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(json.dumps('Token\'s user ID doesn\'t match given user ID.'), 401)
+        response.headers['Content-type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(json.dumps('Token\'s client DI does not match'), 401)
+        print('Token\'s client ID does not match app\'s')
+        response.headers['Content-type'] = 'application/json'
+        return response
+
+    # Check to see if user is already logged in
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response =  make_response(json.dumps('Current user is already connected'), 200)
+        response.headers['Content-type'] = 'application/json'
+
+    # Store the access token in the session for later use
+    login_session['creadentials'] = credentials
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
+    params = { 'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = json.loads(answer.text)
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome '
+    output += login_session['username']
+
+    output += '</h1>'
+    output += '<img src=\''
+    output += login_session['picture']
+    output += '\'style=\'width: 300px; height: 300px; ' \
+              'border-radius: 150px; -webkit-border-radius: ' \
+              '150px; -moz-border-radius: 150px;\'>'
+    flash('you are now logged in as %s' % login_session['username'])
+    return output
+
 
 
 # JSON APIs to view Restaurant Information
